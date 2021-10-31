12e7056a7d2f8a918995b92c1bd3c0fe29f96fd8
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-31 13:51:57 +0300

    It will store the news api base url

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_API_BASE_URL="https://newsapi.org/v2/everything?q=Apple&from=2021-10-31&sortBy=popularity&apiKey={}"
 
 
 
