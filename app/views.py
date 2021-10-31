fb9dde4d4a61496e9309edb509a061446fa478ee
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-31 13:35:21 +0300

    It will create a dynamic route that views a news page using news id

--- app/views.py
+++ app/views.py
@@ -10,3 +10,11 @@ def index():
     '''
     news = "News API"
     return render_template('index.html', news=news)
+
+@app.route('/news/<news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news article
+    '''
+    return render_template('news.html',id = news_id)
