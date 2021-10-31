07b7739d530b0d4348f3d899581a3832e210b62b
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-31 14:09:41 +0300

    It will add the news objects

---
+++ app/models/news.py
@@ -0,0 +1,13 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,id,name,description,url,category,country,language):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
--- app/models/news_test.py
+++ app/models/news_test.py
@@ -11,7 +11,7 @@ class MovieTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_news = News()
+        self.new_news = News(12, "Apple", "Apple reaches 400 buyers", "https://Apple.com", "Top search", "USA", "English")
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
 
