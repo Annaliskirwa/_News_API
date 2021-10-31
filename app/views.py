7a6239b26ebbfb2f07f4f5bbdb8a9825cdbefc96
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-31 13:31:19 +0300

    It will create a variable for the news

--- app/views.py
+++ app/views.py
@@ -8,4 +8,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    news = "News API"
+    return render_template('index.html', news=news)
