0ed5f5a835ebb18c34236cffd228d964c7722d46
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-31 15:07:22 +0300

    Displayin the results

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
@@ -8,8 +9,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    sources = get_news('sources')
     news = "News API"
-    return render_template('index.html', news=news)
+    return render_template('index.html', news=news, sources=sources)
 
 @app.route('/news/<news_id>')
 def news(news_id):
