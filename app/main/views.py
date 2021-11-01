6fb4bf18223af0971aa0787d1659864f0c12cc5b
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-01 16:40:17 +0300

    It will edit the navbar and the titles colors

--- app/main/views.py
+++ app/main/views.py
@@ -13,7 +13,7 @@ def index():
 	sports_sources = get_sources('sports')
 	technology_sources = get_sources('technology')
 	entertainment_sources = get_sources('entertainment')
-	title = "News Highlighter"
+	title = "News API"
 
 	return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
 
